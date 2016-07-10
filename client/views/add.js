10f39ffd7530c9a8684dfacd4beeb9a374401cbb
Paulo Truta <pinheirotruta5@gmail.com>
2016-07-10 01:56:44 +0100

    Track preview before inserting (part 2 of #8)

--- client/views/add.js
+++ client/views/add.js
@@ -27,6 +27,36 @@ Template.add.helpers({
 	},
 	'addingMusic': function(){
 		return Session.get('addingMusic');
+	},
+	'youtubePlayerHelper': function(){
+		// YouTube API will call onYouTubeIframeAPIReady() when API ready.
+    	// Make sure it's a global variable.
+    	onYouTubeIframeAPIReady = function () {
+
+        	// New Video Player, the first argument is the id of the div.
+        	// Make sure it's a global variable.
+        	player = new YT.Player("youtubePlayer", {
+
+            	height: "400", 
+            	width: "600", 
+
+            	// videoId is the "v" in URL (ex: http://www.youtube.com/watch?v=LdH1hSWGFGU, videoId = "LdH1hSWGFGU")
+            	videoId: Session.get('videoObject_tmp').video_id, 
+
+            	// Events like ready, state change, 
+            	events: {
+
+                	onReady: function (event) {
+                    	// Play video when player ready.
+                    	event.target.playVideo();
+                	}
+
+            	}
+
+        	});
+    	};
+
+    	YT.load();
 	}
 });
 
