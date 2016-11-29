69fdd1c580b660e318d1a85c7f26c69f75ac3068
Paulo Truta <jpt@eurotux.com>
2016-11-29 23:33:26 +0000

    :sparkles: New home screen config prototype

--- client/views/home.js
+++ client/views/home.js
@@ -1,5 +1,25 @@
 Template.home.helpers({
-	'homeHelperExample1': function(){
+	'featuredArtists': function(){
+		// var sample = Meteor.music.find({}, {}, 0, 10).fetch();
+		// /**
+		//  * Randomize array element order in-place.
+		//  * Using Durstenfeld shuffle algorithm.
+		//  */
+		// function shuffleArray(array) {
+		//     for (var i = array.length - 1; i > 0; i--) {
+		//         var j = Math.floor(Math.random() * (i + 1));
+		//         var temp = array[i];
+		//         array[i] = array[j];
+		//         array[j] = temp;
+		//     }
+		//     return array;
+		// }
+
+		// sample = shuffleArray(sample);
+
+		// console.log(sample);
+
+		
 
 	},
 });
@@ -24,5 +44,14 @@ Template.home.events({
 });
 
 Template.home.rendered = function () {
+	$('.slider').slider({
+		full_width: true,
+		indicators: false,
+		height: 250,
+		interval: 3000
+	});
+
+	// pause after 3 seconds...
+	Meteor.setTimeout(function(){ $('.slider').slider('pause'); }, 5000);
 
 };
