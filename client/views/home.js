212da481df58f526f5aa4cfb5402c24889a8989c
Paulo Truta <pinheirotruta5@gmail.com>
2017-03-29 22:32:59 +0000

    Roolback and modules

--- client/views/home.js
+++ client/views/home.js
@@ -24,5 +24,14 @@ Template.home.events({
 });
 
 Template.home.rendered = function () {
+	$('.slider').slider({
+		full_width: true,
+		indicators: false,
+		height: 250,
+		interval: 3000
+	});
+
+	// pause after 3 seconds...
+		Meteor.setTimeout(function(){ $('.slider').slider('pause'); }, 5000);
 
 };
